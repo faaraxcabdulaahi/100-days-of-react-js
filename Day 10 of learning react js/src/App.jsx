9602f48d536c119514185fe5d1c376badb96e597
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-03-04 23:00:23 +0300

    Finish the day 10 of learning react js by building User-Registration Form project

---
+++ Day 10 of learning react js/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignUpAndLogin from "./components/SignUpAndLogin"
+
+const App = () => {
+  return <div>
+    <SignUpAndLogin/>
+  </div>;
+};
+
+export default App;
